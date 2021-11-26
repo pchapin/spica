23ce60508e80495c46c7a672a43a3780110bfa51
Peter Chapin <chapinp@acm.org>
2021-11-26 10:36:11 -0500

    Implemented RedBlack Trees.

--- Ada/spica-redblack_trees.adb
+++ Ada/spica-redblack_trees.adb
@@ -28,10 +28,111 @@ package body Spica.RedBlack_Trees is
    end Left_Rotate;
 
 
-   -- TODO: Finish Me!
-   procedure Insert(T : in out RedBlack_Tree; Key : in Key_Type; Value : in Value_Type) is
+   procedure Right_Rotate(T : in out RedBlack_Tree; Y : in Tree_Node_Access)
+     with Pre => Y.Left_Child /= T.Nil
+   is
+      X : constant Tree_Node_Access := Y.Left_Child;
    begin
-      raise Program_Error;
+      Y.Left_Child := X.Right_Child;
+      if X.Right_Child /= T.Nil then
+         X.Right_Child.Parent := Y;
+      end if;
+      X.Parent := Y.Parent;
+      if Y.Parent = T.Nil then
+         T.Root := X;
+      elsif Y = Y.Parent.Left_Child then
+         Y.Parent.Left_Child := X;
+      else
+         Y.Parent.Right_Child := X;
+      end if;
+      X.Right_Child := Y;
+      Y.Parent := X;
+   end Right_Rotate;
+
+
+   procedure RedBlack_Insert_Fixup(T : in out RedBlack_Tree; Z : in out Tree_Node_Access) is
+      Y : Tree_Node_Access;
+   begin
+      while Z.Parent.Color = Red loop
+         if Z.Parent = Z.Parent.Parent.Left_Child then
+            Y := Z.Parent.Parent.Right_Child;
+            if Y.Color = Red then
+               -- Case #1
+               Z.Parent.Color := Black;
+               Y.Color := Black;
+               Z.Parent.Parent.Color := Red;
+               Z := Z.Parent.Parent;
+            else
+               if Z = Z.Parent.Right_Child then
+                  -- Case #2
+                  Z := Z.Parent;
+                  Left_Rotate(T, Z);
+               end if;
+               -- Case #3
+               Z.Parent.Color := Black;
+               Z.Parent.Parent.Color := Red;
+               Right_Rotate(T, Z.Parent.Parent);
+            end if;
+         else
+            Y := Z.Parent.Parent.Left_Child;
+            if Y.Color = Red then
+               -- Case #1
+               Z.Parent.Color := Black;
+               Y.Color := Black;
+               Z.Parent.Parent.Color := Red;
+               Z := Z.Parent.Parent;
+            else
+               if Z = Z.Parent.Left_Child then
+                  -- Case #2
+                  Z := Z.Parent;
+                  Right_Rotate(T, Z);
+               end if;
+               -- Case #3
+               Z.Parent.Color := Black;
+               Z.Parent.Parent.Color := Red;
+               Left_Rotate(T, Z.Parent.Parent);
+            end if;
+         end if;
+      end loop;
+      T.Root.Color := Black;
+   end RedBlack_Insert_Fixup;
+
+
+   procedure Insert(T : in out RedBlack_Tree; Key : in Key_Type; Value : in Value_Type) is
+      X : Tree_Node_Access;
+      Y : Tree_Node_Access;
+      Z : Tree_Node_Access := new Tree_Node'(Key, Value, Red, others => <>);
+   begin
+      Y := T.Nil;
+      X := T.Root;
+      while X /= T.Nil loop
+
+         -- If the key already exists, replace the value and we are done.
+         if Key = X.Key then
+            X.Value := Value;
+            return;
+         end if;
+
+         -- Otherwise search down the tree.
+         Y := X;
+         if Key < X.Key then
+            X := X.Left_Child;
+         else
+            X := X.Right_Child;
+         end if;
+      end loop;
+      Z.Parent := Y;
+      if Y = T.Nil then
+         T.Root := Z;
+      elsif Key < Y.Key then
+         Y.Left_Child := Z;
+      else
+         Y.Right_Child := Z;
+      end if;
+      Z.Left_Child := T.Nil;
+      Z.Right_Child := T.Nil;
+      RedBlack_Insert_Fixup(T, Z);
+      T.Count := T.Count + 1;
    end Insert;
 
 
@@ -59,10 +160,158 @@ package body Spica.RedBlack_Trees is
    end Search;
 
 
+   procedure RedBlack_Transplant(T : in out RedBlack_Tree; U, V : in out Tree_Node_Access) is
+   begin
+      if U.Parent = T.Nil then
+         T.Root := V;
+      elsif U = U.Parent.Left_Child then
+         U.Parent.Left_Child := V;
+      else
+         U.Parent.Right_Child := V;
+      end if;
+      V.Parent := U.Parent;
+   end RedBlack_Transplant;
+
+
+   procedure RedBlack_Delete_Fixup(T : in out RedBlack_Tree; Incoming_X : in Tree_Node_Access) is
+      X : Tree_Node_Access := Incoming_X;
+      W : Tree_Node_Access;
+   begin
+      while X /= T.Root and X.Color = Black loop
+         if X = X.Parent.Left_Child then
+            W := X.Parent.Right_Child;
+            if W.Color = Red then
+               -- Case #1
+               W.Color := Black;
+               X.Parent.Color := Red;
+               Left_Rotate(T, X.Parent);
+               W := X.Parent.Right_Child;
+            end if;
+            if W.Left_Child.Color = Black and W.Right_Child.Color = Black then
+               -- Case #2
+               W.Color := Red;
+               X := X.Parent;
+            else
+               if W.Right_Child.Color = Black then
+                  -- Case #3
+                  W.Left_Child.Color := Black;
+                  W.Color := Red;
+                  Right_Rotate(T, W);
+                  W := X.Parent.Right_Child;
+               end if;
+               -- Case #4
+               W.Color := X.Parent.Color;
+               X.Parent.Color := Black;
+               W.Right_Child.Color := Black;
+               Left_Rotate(T, X.Parent);
+               X := T.Root;
+            end if;
+         else
+            W := X.Parent.Left_Child;
+            if W.Color = Red then
+               -- Case #1
+               W.Color := Black;
+               X.Parent.Color := Red;
+               Right_Rotate(T, X.Parent);
+               W := X.Parent.Left_Child;
+            end if;
+            if W.Right_Child.Color = Black and W.Left_Child.Color = Black then
+               -- Case #2
+               W.Color := Red;
+               X := X.Parent;
+            else
+               if W.Left_Child.Color = Black then
+                  -- Case #3
+                  W.Right_Child.Color := Black;
+                  W.Color := Red;
+                  Left_Rotate(T, W);
+                  W := X.Parent.Left_Child;
+               end if;
+               -- Case #4
+               W.Color := X.Parent.Color;
+               X.Parent.Color := Black;
+               W.Left_Child.Color := Black;
+               Right_Rotate(T, X.Parent);
+               X := T.Root;
+            end if;
+         end if;
+      end loop;
+      X.Color := Black;
+   end RedBlack_Delete_Fixup;
+
+
    -- TODO: Finish Me!
    procedure Delete(T : in out RedBlack_Tree; Key : in Key_Type) is
+
+      function Search_For_Key(Key : in Key_Type) return Tree_Node_Access is
+         Pointer : Tree_Node_Access := T.Root;
+      begin
+         -- Search down the tree starting at the root.
+         while Pointer /= T.Nil loop
+            -- If we found what we want, we are done.
+            if Key = Pointer.Key then
+               return Pointer;
+            end if;
+
+            -- Otherwise step in the correct direction.
+            if Key < Pointer.Key then
+               Pointer := Pointer.Left_Child;
+            else
+               Pointer := Pointer.Right_Child;
+            end if;
+         end loop;
+         return T.Nil;
+      end Search_For_Key;
+
+      function Tree_Minimum(X : in Tree_Node_Access) return Tree_Node_Access
+        with Pre => X /= T.Nil
+      is
+         Result : Tree_Node_Access := X;
+      begin
+         while Result.Left_Child /= T.Nil loop
+            Result := Result.Left_Child;
+         end loop;
+         return Result;
+      end Tree_Minimum;
+
+      X : Tree_Node_Access;
+      Y : Tree_Node_Access;
+      Z : Tree_Node_Access;
+      Y_Original_Color : Color_Type;
    begin
-      raise Program_Error;
+      Z := Search_For_Key(Key);
+
+      -- If the Key is not present, we are done. Skip the body of this procedure.
+      if Z /= T.Nil then
+         Y := Z;
+         Y_Original_Color := Y.Color;
+         if Z.Left_Child = T.Nil then
+            X := Z.Right_Child;
+            RedBlack_Transplant(T, Z, Z.Right_Child);
+         elsif Z.Right_Child = T.Nil then
+            X := Z.Left_Child;
+            RedBlack_Transplant(T, Z, Z.Left_Child);
+         else
+            Y := Tree_Minimum(Z.Right_Child);
+            Y_Original_Color := Y.Color;
+            X := Y.Right_Child;
+            if Y.Parent = Z then
+               X.Parent := Y;
+            else
+               RedBlack_Transplant(T, Y, Y.Right_Child);
+               Y.Right_Child := Z.Right_Child;
+               Y.Right_Child.Parent := Y;
+            end if;
+            RedBlack_Transplant(T, Z, Y);
+            Y.Left_Child := Z.Left_Child;
+            Y.Left_Child.Parent := Y;
+            Y.Color := Z.Color;
+         end if;
+         if Y_Original_Color = Black then
+            RedBlack_Delete_Fixup(T, X);
+         end if;
+         T.Count := T.Count - 1;
+      end if;
    end Delete;
 
 
